27b761677e2f15edf013476c6224cd584e8a3314
David <davidcastellobatet@gmail.com>
2022-10-20 09:35:41 +0200

    Estructura i funcionalitat swiper

--- lib/widgets/card_swiper_widget.dart
+++ lib/widgets/card_swiper_widget.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,12 +6,31 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final sizeHeight = MediaQuery.of(context).size.height;
+    final size = MediaQuery.of(context).size;
 
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      height: sizeHeight * 0.5,
-      color: Colors.red,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.45,
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: (() => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-list')),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('images/no-image.jpg'),
+                image: NetworkImage('https//via.placeholder.com/300x400'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
